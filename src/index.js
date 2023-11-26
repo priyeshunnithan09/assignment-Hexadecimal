1f435bb5b3f0d458a3d0a9fb0007df088c7cba31
priyeshunnithan09 <summersid9@gmail.com>
2023-11-26 22:41:45 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  const [usersData, setUsersData] = useState([]);
+  const [searchText, setSearchText] = useState('');
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  useEffect(() => {
+    // Fetch data from the backend
+    axios.get(`http://localhost:3001/v1/users?searchText=${searchText}`)
+      .then(response => setUsersData(response.data))
+      .catch(error => console.error(error));
+  }, [searchText]);
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+      />
+
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Posts</th>
+          </tr>
+        </thead>
+        <tbody>
+          {usersData.map(user => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>
+                <ul>
+                  {user.posts.map(post => (
+                    <li key={post.id}>{post.title}</li>
+                  ))}
+                </ul>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
